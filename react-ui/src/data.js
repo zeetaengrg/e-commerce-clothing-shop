6657832d1331e0a7c2696e912f7618baa9a9ada6
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-11 18:51:34 +0545

    feat(data.js): data.js local database added

---
+++ react-ui/src/data.js
@@ -0,0 +1,20 @@
+export const slideItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/QP3qV5Y/slider1.png",
+        title: "WINTER SALE",
+        description: "DON'T MISS OUT ON WINTER SALE. GET FLAT 30% OFF!!!"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/rMkkLWD/slider2.jpg",
+        title: "NEW ARRIVALS",
+        description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/VD0wXQy/amanda-vick-oh-Wf6-Yuz-OQk-unsplash.jpg",
+        title: "SUMMER SALE",
+        description: "SUMMER SALE. GET FLAT 30% OFF!!!"
+    }
+]
