b3bd040aa92cb09e6769ee3512ec83752947784a
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-11 16:34:44 +0545

    feat(Slider): Slider.jsx component added to Home page

--- react-ui/src/pages/Home.jsx
+++ react-ui/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Announcements from '../components/Announcements';
 import Navbar from '../components/Navbar';
+import Slider from '../components/Slider';
 
 const Home = () => {
     return (
         <div>
             <Announcements />
             <Navbar />
+            <Slider />
         </div>
     )
 }
