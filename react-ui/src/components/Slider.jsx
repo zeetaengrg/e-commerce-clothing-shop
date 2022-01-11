ac65cd5bd90c6684187e05be19e36b2dcf7fc4cb
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-11 21:19:39 +0545

    feat(slider): image slider functionality added

--- react-ui/src/components/Slider.jsx
+++ react-ui/src/components/Slider.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styled from 'styled-components';
-import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
+import { slideItems } from '../data';
+import { ArrowLeft, ArrowRight } from '@mui/icons-material';
 
 const Container = styled.div`
     height: 100vh;
@@ -8,6 +9,7 @@ const Container = styled.div`
     background-color: #f5f5f5;
     display: flex;
     position: relative;
+    overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -21,19 +23,86 @@ const Arrow = styled.div`
     position: absolute;
     top: 0;
     bottom: 0;
-    left: ${props => props.direction === 'back' && '1rem'};
-    right: ${props => props.direction === 'forward' && '1rem'};
+    left: ${props => props.direction === 'left' && '1rem'};
+    right: ${props => props.direction === 'right' && '1rem'};
     margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
+    z-index: 2;
+    :hover {
+        opacity: 0.2;
+    }
 `;
 
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    transform: translateX(${props => props.currentSlide * -100}vw);
+    transition: all 1.5s ease;
+`;
+
+const Slide = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+`;
+
+const ImageContainer = styled.div`
+    flex: 1;
+`;
+
+const Image = styled.img`
+    height: 100vh;
+    width: 100vw;
+`;
+
+const Button = styled.div`
+    height: 1.5rem;
+    width: 5rem;
+    border: 1px solid black;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+    position: absolute;
+    z-index: 2;
+    bottom: 1rem;
+    margin: 0 50vw;
+    cursor: pointer;
+    :hover {
+        color: #707070;
+        border-color: #707070;
+    }
+`;
+
+
 const Slider = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const handleClick = (direction) => {
+        if (direction === 'left') {
+            setCurrentSlide(currentSlide > 0 ? currentSlide - 1: slideItems.length - 1);
+        } else {
+            setCurrentSlide(currentSlide < slideItems.length -1 ? currentSlide + 1: 0);
+        }
+    };
+
     return (
         <Container>
-            <Arrow direction="back">
-                <ArrowBackIos />
+            <Arrow direction="left" onClick={() => handleClick("left")}>
+                <ArrowLeft />
             </Arrow>
-            <Arrow direction="forward">
-                <ArrowForwardIos />
+            <Wrapper currentSlide={currentSlide}>
+                {slideItems.map(item => (
+                    <Slide>
+                        <ImageContainer>
+                            <Image src={item.img} />
+                        </ImageContainer>
+                        <Button>SHOP NOW</Button>
+                    </Slide>
+                ))}
+            </Wrapper>
+            <Arrow direction="right" onClick={() => handleClick("right")}>
+                <ArrowRight />
             </Arrow>
         </Container>
     )
