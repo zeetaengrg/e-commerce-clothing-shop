33db97286b34f9c26af7a082143017fb5f327952
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-11 14:38:56 +0545

    refactor(styled-components): styled components installed into respective directory

--- react-ui/src/components/Navbar.jsx
+++ react-ui/src/components/Navbar.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import { SearchIcon } from '@mui/icons-material';
 
 const Container = styled.div`
-    height: 100px;
-    background-color: red;
+    height: 60px;
 `;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Left = styled.div``
+
+const Language = styled.div`
+    font-size: 1rem;
+    cursor: pointer;
+`;
+
+const SearchContainer = styled.div``;
+
+const Center = styled.div``
+const Right = styled.div``
+
 
 const Navbar = () => {
     return (
         <Container>
-            Navbar
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <input type="text" />
+                        <SearchIcon />
+                    </SearchContainer>
+                </Left>
+                <Center>Center</Center>
+                <Right>Right</Right>
+            </Wrapper>
         </Container>
     )
 };
