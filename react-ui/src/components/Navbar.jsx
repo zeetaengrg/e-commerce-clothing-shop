193b7351a5a3517e3c940e84835cee35caa2883f
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-11 15:54:37 +0545

    feat(Navbar): Navbar items added and styled accordingly
    Language item added to the left.
    Search Button added.
    
    Brand Logo added to the center.
    
    Register, Sign in and cart items added to the right.

--- react-ui/src/components/Navbar.jsx
+++ react-ui/src/components/Navbar.jsx
@@ -1,29 +1,74 @@
 import React from 'react';
 import styled from 'styled-components';
-import { SearchIcon } from '@mui/icons-material';
+import { Search, ShoppingCartOutlined } from '@mui/icons-material';
+import { Badge } from '@mui/material';
 
 const Container = styled.div`
-    height: 60px;
+    height: 50px;
 `;
 
 const Wrapper = styled.div`
-    padding: 10px 20px;
+    padding: 0px 20px;
     display: flex;
     justify-content: space-between;
+    align-items: center;
 `;
 
-const Left = styled.div``
+const Left = styled.div`
+    display: flex;
+    flex: 1;
+    gap: 1rem;
+`;
 
 const Language = styled.div`
     font-size: 1rem;
     cursor: pointer;
+    align-items: center;
+    display: flex;
 `;
 
-const SearchContainer = styled.div``;
+const SearchContainer = styled.div`
+    border: 1px solid #ccc;
+    border-radius: 0.3rem;
+    display: flex;
+    align-items: center;
+    margin-left: 0.5rem;
+    color: #ccc;
+`;
 
-const Center = styled.div``
-const Right = styled.div``
+const Input = styled.input`
+    border: none;
+    outline: none;
+    background: transparent;
+    font-size: 1rem;
+    padding-left: 0.2rem;
+`;
 
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+
+const Logo = styled.h1`
+    font-size: 2.5rem;
+    font-weight: bold;
+    cursor: pointer;
+    text-transform: uppercase;
+`;
+
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    gap: 1rem;
+    justify-content: flex-end;
+    text-transform: uppercase;
+    align-items: center;
+`;
+
+const MenuItem = styled.div`
+    font-size: 1rem;
+    cursor: pointer;
+`;
 
 const Navbar = () => {
     return (
@@ -32,12 +77,22 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        <input type="text" />
-                        <SearchIcon />
+                        <Input placeholder="Search" />
+                        <Search />
                     </SearchContainer>
                 </Left>
-                <Center>Center</Center>
-                <Right>Right</Right>
+                <Center>
+                    <Logo>Logo</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>Register</MenuItem>
+                    <MenuItem>Sign In</MenuItem>
+                    <MenuItem>
+                        <Badge color="primary" badgeContent={4} >
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
         </Container>
     )
