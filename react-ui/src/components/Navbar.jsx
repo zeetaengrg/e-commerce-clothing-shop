4d78851facf5d0ea234e2ca2c68d60bdbf106933
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-11 12:20:06 +0545

    feat(Navbar): Navbar component added

---
+++ react-ui/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100px;
+    background-color: red;
+`;
+
+const Navbar = () => {
+    return (
+        <Container>
+            Navbar
+        </Container>
+    )
+};
+
+export default Navbar;
