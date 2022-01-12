b9f6ccb4cd9ea769e20595c9f0ed5ccf9a9a6cad
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-12 15:41:25 +0545

    feat(categories): categories data added

--- react-ui/src/data.js
+++ react-ui/src/data.js
@@ -2,37 +2,55 @@ export const slideItems = [
     {
         id: 1,
         img: "https://i.ibb.co/QP3qV5Y/slider1.png",
-        title: "WINTER SALE",
-        description: "DON'T MISS OUT ON WINTER SALE. GET FLAT 30% OFF!!!"
+        title: "NEW ARRIVALS!!!",
+        description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!"
     },
     {
         id: 2,
         img: "https://i.ibb.co/rMkkLWD/slider2.jpg",
-        title: "NEW ARRIVALS",
+        title: "NEW ARRIVALS!!!",
         description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!"
     },
     {
         id: 3,
         img: "https://i.ibb.co/VD0wXQy/amanda-vick-oh-Wf6-Yuz-OQk-unsplash.jpg",
-        title: "SUMMER SALE",
-        description: "SUMMER SALE. GET FLAT 30% OFF!!!"
+        title: "WINTER SALE!!!",
+        description: "DON'T MISS OUT ON WINTER SALE. GET FLAT 30% OFF!!!"
     },
     {
         id: 4,
         img: "https://i.ibb.co/1JSpLfc/priscilla-du-preez-dlx-LGIy-2-VU-unsplash.jpg",
-        title: "SUMMER SALE",
-        description: "SUMMER SALE. GET FLAT 30% OFF!!!"
+        title: "SUMMER SALE!!!",
+        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!"
     },
     {
         id: 5,
         img: "https://i.ibb.co/Tkg1BLk/the-creative-exchange-k-WVIm-L5-Qx-JI-unsplash.jpg",
-        title: "SUMMER SALE",
-        description: "SUMMER SALE. GET FLAT 30% OFF!!!"
+        title: "SUMMER SALE!!!",
+        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!"
     },
     {
         id: 6,
         img: "https://i.ibb.co/PcQcFsn/alyssa-strohmann-TS-u-Nw-Jq-E-unsplash.jpg",
-        title: "SUMMER SALE",
-        description: "SUMMER SALE. GET FLAT 30% OFF!!!"
+        title: "WINTER SALE!!!",
+        description: "DON'T MISS OUT ON WINTER SALE. GET FLAT 30% OFF!!!"
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/tmR0VfM/pants-3737416-1920.jpg",
+        title: "JEANS"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/q09G3KP/kemal-alkan-BDBEP0e-PQc-unsplash.jpg",
+        title: "JACKETS"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/CWs8x2Y/faith-yarn-Wr0-Tp-Kqf26s-unsplash.jpg",
+        title: "T-SHIRTS"
     }
 ];
