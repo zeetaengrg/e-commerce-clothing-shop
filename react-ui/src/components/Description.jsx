dccfbf073364f5e61d78bdced8f877c260f0e154
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-12 19:11:35 +0545

    refactor(Description): Description component added for reusability

---
+++ react-ui/src/components/Description.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Desc = styled.p`
+    background-color: #fff;
+    opacity: 0.5;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+`;
+
+const Description = ({item}) => {
+    return (
+        <>
+            <Desc>
+                {item.description}
+            </Desc>    
+        </>
+    )
+}
+
+export default Description;
