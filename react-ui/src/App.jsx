e930fa29b901e8100bfda739d086fe0757053320
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-13 17:05:26 +0545

    refactor

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import SingleProduct from "./pages/SingleProduct";
 
 const App = () => {
   return (
     <div className="App">
       {/* <Home /> */}
-      <ProductList />
+      {/* <ProductList /> */}
+      <SingleProduct />
     </div>
   )
 }
