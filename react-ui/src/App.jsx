31e828aa69da6055721700bc55cb6ce83677a8d4
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-17 21:01:00 +0545

    refactor: Cart page added

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,13 +1,15 @@
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import SingleProduct from "./pages/SingleProduct";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
       {/* <ProductList /> */}
       {/* <SingleProduct /> */}
+      <Cart />
     </div>
   )
 }
