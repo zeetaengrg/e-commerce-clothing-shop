0a05d2f65689fa58fb6032e1a8a302848130e813
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-17 14:52:04 +0545

    refactor: console.log commented

--- react-ui/src/components/SignUpModal.jsx
+++ react-ui/src/components/SignUpModal.jsx
@@ -59,12 +59,12 @@ const SignUpModal = () => {
     })
 
     const onSubmit = (values, props) => {
-        console.log(values)
+        // console.log(values)
         setTimeout(() => {
             props.resetForm();
             props.setSubmitting(false);
         }, 2000)
-        console.log(props);
+        // console.log(props);
     };
 
     return (
