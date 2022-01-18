76268584bdb908b12aca0d69e7c6c82d8a74adb8
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-18 09:30:57 +0545

    feat: App.css added to App.jsx component
    - App.css styling has been added to App.jsx
    - This styling provides warning text in red color during form validation.

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import SingleProduct from "./pages/SingleProduct";
 import Cart from "./pages/Cart";
+import "./App.css";
 
 const App = () => {
   return (
