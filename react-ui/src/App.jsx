dda26f0075418e592b68dd5f992e25da50036cfa
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-01-30 21:09:27 +0545

    refactor

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -7,10 +7,10 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      {/* <Home /> */}
+      <Home />
       {/* <ProductList /> */}
       {/* <SingleProduct /> */}
-      <Cart />
+      {/* <Cart /> */}
     </div>
   )
 }
