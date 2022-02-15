3b5b99623994c19f138eb59a897e1c35a87272ee
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-15 20:36:09 +0545

    feat(folder structure): directories created and assigned accordingly

--- react-ui/src/pages/Home/Home.jsx
+++ react-ui/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Announcements from '../../components/Announcements/Announcements';
-import Navbar from '../../components/Navbar/Navbar';
-import Slider from '../../components/Slider/Slider';
-import Categories from '../../components/Categories/Categories';
-import NewArrivalCategory from '../../components/Categories/NewArrivalCategory';
-import Newsletter from '../../components/Newsletter/Newsletter';
-import Footer from '../../components/Footer/Footer';
+import Announcements from '../../components/elements/Announcements/Announcements';
+import Navbar from '../../components/layouts/Navbar/Navbar';
+import Slider from '../../components/modules/Slider/Slider';
+import Categories from '../../components/modules/Categories/Categories';
+import NewArrivalCategory from '../../components/modules/Categories/NewArrivalCategory/NewArrivalCategory';
+import Newsletter from '../../components/elements/Newsletter/Newsletter';
+import Footer from '../../components/layouts/Footer/Footer';
 
 const Home = () => {
     return (
