535a3be1f361dad6d1b94ab15d9a9a636a288754
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-20 19:18:07 +0545

    style(styled-components): new styled components created
    - Form.styles.jsx contains common Form styling components.
    - SignInModal.styles.jsx contains styling for SignInModal only.
    - SignUpModal.styles.jsx contains styling for SignUpModal only.

---
+++ react-ui/src/components/modules/Form/SignInModal/SignInModal.styles.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const FooterText = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ForgotText = styled.a`
+    text-decoration: underline;
+    color: #1565C0;
+    cursor: pointer;
+`;
+
+export const AccountText = styled.text`
+    
+`;
+
+export const SignUpLink = styled.a`
+    margin-left: 0.5rem;
+    text-decoration: underline;
+    color: #1565C0;
+    cursor: pointer;
+`;
---
+++ react-ui/src/components/modules/Form/SignUpModal/SignUpModal.styles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const FormLabelTitle = styled.p`
+    font-size: 1.1rem;
+    color: grey;
+`;
+
+export const FormLabel = styled.div`
+    display: flex;
+    gap: 0.5rem;
+`;
+
+export const Label = styled.label`
+    font-size: 1.1rem;
+`;
