ffc999e64c6bb33eb9c05be8cef12c1e8a6c804a
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-20 19:02:08 +0545

    feat(signup): material ui components replaced

--- react-ui/src/components/modules/Form/SignUpModal/SignUpModal.jsx
+++ react-ui/src/components/modules/Form/SignUpModal/SignUpModal.jsx
@@ -1,30 +1,24 @@
 import * as React from 'react';
-import { Avatar, Button, Checkbox, FormControlLabel, FormHelperText, FormLabel, Grid, Paper, Radio, RadioGroup, TextField, Typography } from "@mui/material";
 import { MdAddCircleOutline } from 'react-icons/md';
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import {
+    FormLabelTitle,
+    FormLabel,
+    Label,
+} from "./SignUpModal.styles";
+import {
+    Container,
+    HeaderContent,
+    Icon,
+    HeaderText,
+    TextField,
+    Error,
+    FormControl,
+    Button,
+} from "../Form.styles";
 
 const SignUpModal = () => {
-
-    const paperStyle = {
-        width: 300,
-        backgroundColor: 'background.paper',
-        margin: '0 auto',
-        padding: '20px'
-    };
-
-    const btnStyle = {
-        fontFamily: "inherit",
-        fontSize: "1rem",
-    };
-
-    const avatarStyle = {
-        backgroundColor: 'rgb(245, 0, 87)',
-    };
-
-    const textFieldStyle = {
-        margin: "0.3rem 0",
-    };
 
     const initialValues = {
         firstName: '',
@@ -51,168 +45,153 @@ const SignUpModal = () => {
         password: Yup.string()
             .min(8, 'Password must be at least 8 characters!')
             .max(25, 'Password must be at most 25 characters!')
+            .matches(/[a-z]/, 'Password must one lowercase!')
+            .matches(/[A-Z]/, 'Password must one uppercase!')
+            .matches(/[0-9]/, 'Password must one number!')
             .required('Required'),
         confirmPassword: Yup.string()
             .oneOf([Yup.ref('password')], 'Passwords must match!')
             .required('Required'),
-        termsAndCondition: Yup.boolean(["true"], "Accept Terms and Conditions").required('Required'),
+        termsAndCondition: Yup.boolean().oneOf([true], "You must Accept Terms and Conditions!")
     })
 
-    const onSubmit = (values, props) => {
+    const onSubmit = (values, { setSubmitting, resetForm }) => {
         setTimeout(() => {
-            props.resetForm();
-            props.setSubmitting(false);
-        }, 2000)
+            resetForm(initialValues);
+            setSubmitting(false);
+        }, 1500)
     };
 
     return (
         <React.Fragment>
-            <Grid>
-                <Paper elevation={20} sx={paperStyle}>
-                    <Grid align="center">
-                        <Avatar sx={avatarStyle}>
-                            <MdAddCircleOutline />
-                        </Avatar>
-                        <Typography variant="h4" sx={{ marginTop: "0.5rem" }}>
-                            Sign Up
-                        </Typography>
-                    </Grid>
-                    <Formik
-                        initialValues={initialValues}
-                        onSubmit={onSubmit}
-                        validationSchema={validationSchema}
-                    >
-                        {(props) => (
-                            <Form>
+            <Container>
+                <HeaderContent>
+                    <Icon>
+                        <MdAddCircleOutline />
+                    </Icon>
+                    <HeaderText>
+                        Sign Up
+                    </HeaderText>
+                </HeaderContent>
+                <Formik
+                    initialValues={initialValues}
+                    onSubmit={onSubmit}
+                    validationSchema={validationSchema}
+                >
+                    {({ isSubmitting }) => (
+                        <Form style={{ marginTop: "1.5rem" }}>
+                            <FormControl>
                                 <Field
                                     as={TextField}
-                                    sx={textFieldStyle}
                                     name="firstName"
                                     label="First Name"
-                                    placeholder="Enter First Name"
                                     type="text"
-                                    size="small"
-                                    helperText={
-                                        <ErrorMessage name="firstName" />
-                                    }
-                                    fullWidth
-                                    required
+                                    placeholder="First Name"
                                 />
-                                <Field
+                                <Error>
+                                    <ErrorMessage name="firstName" />
+                                </Error>
+                            </FormControl>
+                            <FormControl>
+                                <Field 
                                     as={TextField}
-                                    sx={textFieldStyle}
                                     name="lastName"
                                     label="Last Name"
-                                    placeholder="Enter Last Name"
                                     type="text"
-                                    size="small"
-                                    helperText={
-                                        <ErrorMessage name="lastName" />
-                                    }
-                                    fullWidth
-                                    required
+                                    placeholder="Last Name"
                                 />
-                                <Field
+                                <Error>
+                                    <ErrorMessage name="lastName" />
+                                </Error>
+                            </FormControl>
+                            <FormControl>
+                                <Field 
                                     as={TextField}
-                                    sx={textFieldStyle}
                                     name="email"
                                     label="Email"
-                                    placeholder="Enter Email"
                                     type="email"
-                                    size="small"
-                                    helperText={<ErrorMessage name="email" />}
-                                    fullWidth
-                                    required
+                                    placeholder="Email"
                                 />
-                                <FormLabel
-                                    component="legend"
-                                    style={{ marginTop: "0.3rem" }}
-                                >
+                                <Error>
+                                    <ErrorMessage name="email" />
+                                </Error>
+                            </FormControl>
+                            <FormControl>
+                                <FormLabelTitle>
                                     Gender
+                                </FormLabelTitle>
+                                <FormLabel>
+                                    <Label>
+                                        <Field
+                                            type="radio"
+                                            name="gender"
+                                            value="Male"
+                                            style={{ marginRight:"0.5rem" }}
+                                        />
+                                        Male
+                                    </Label>
+                                    <Label>
+                                        <Field
+                                            type="radio"
+                                            name="gender"
+                                            value="Female"
+                                            style={{ marginRight:"0.5rem" }}
+                                        />
+                                        Female
+                                    </Label>
+                                    <Label>
+                                        <Field
+                                            type="radio"
+                                            name="gender"
+                                            value="Other"
+                                            style={{ marginRight:"0.5rem" }}
+                                        />
+                                        Other
+                                    </Label>
                                 </FormLabel>
-                                <Field
-                                    as={RadioGroup}
-                                    aria-label="gender"
-                                    defaultValue="male"
-                                    name="gender "
-                                    style={{ display: "initial" }}
-                                >
-                                    <FormControlLabel
-                                        value="male"
-                                        control={<Radio />}
-                                        label="Male"
-                                    />
-                                    <FormControlLabel
-                                        value="female"
-                                        control={<Radio />}
-                                        label="Female"
-                                    />
-                                    <FormControlLabel
-                                        value="other"
-                                        control={<Radio />}
-                                        label="Other"
-                                    />
-                                </Field>
-                                {/* <FormHelperText><ErrorMessage name='gender' /></FormHelperText> */}
+                            </FormControl>
+                            <FormControl>
                                 <Field
                                     as={TextField}
-                                    sx={textFieldStyle}
                                     name="password"
                                     label="Password"
-                                    placeholder="Enter Password"
                                     type="password"
-                                    size="small"
-                                    helperText={
-                                        <ErrorMessage name="password" />
-                                    }
-                                    fullWidth
-                                    required
+                                    placeholder="Password"
                                 />
+                                <Error>
+                                    <ErrorMessage name="password" />
+                                </Error>
+                            </FormControl>
+                            <FormControl>
                                 <Field
                                     as={TextField}
-                                    sx={textFieldStyle}
                                     name="confirmPassword"
                                     label="Confirm Password"
-                                    placeholder="Enter Password Again"
                                     type="password"
-                                    size="small"
-                                    helperText={
-                                        <ErrorMessage name="confirmPassword" />
-                                    }
-                                    fullWidth
-                                    required
+                                    placeholder="Confirm Password"
                                 />
-                                <FormControlLabel
-                                    control={
-                                        <Field
-                                            as={Checkbox}
-                                            color="primary"
-                                            name="termsAndCondition"
-                                            style={{ margin: "0.3rem 0" }}
-                                        />
-                                    }
-                                    label="By clicking Sign Up, I agree to the Terms and Conditions."
+                                <Error>
+                                    <ErrorMessage name="confirmPassword" />
+                                </Error>
+                            </FormControl>
+                            <Label>
+                                <Field
+                                    style={{ marginRight: "0.5rem"}}
+                                    name="termsAndCondition"
+                                    type="checkbox"
                                 />
-                                <FormHelperText>
+                                By clicking Sign Up, I agree to the Terms and Conditions.
+                                <Error>
                                     <ErrorMessage name="termsAndCondition" />
-                                </FormHelperText>
-                                <Button
-                                    sx={btnStyle}
-                                    variant="contained"
-                                    type="submit"
-                                    color="primary"
-                                    disabled={props.isSubmitting}
-                                    fullWidth
-                                >
-                                    {props.isSubmitting
-                                        ? "Loading..."
-                                        : "Sign Up"}
-                                </Button>
-                            </Form>
-                        )}
-                    </Formik>
-                </Paper>
-            </Grid>
+                                </Error>
+                            </Label>
+                            <Button type="submit" disabled={isSubmitting}>
+                                {isSubmitting ? 'Submitting...' : 'Sign Up'}
+                            </Button>
+                        </Form>
+                    )}
+                </Formik>
+            </Container>
         </React.Fragment>
     );
 };
