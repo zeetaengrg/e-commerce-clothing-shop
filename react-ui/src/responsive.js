220ffe7c344d2ee7c618cf412cd4d2100dedb2d9
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-21 14:38:20 +0545

    feat(tablet): responsive for tablet created

--- react-ui/src/responsive.js
+++ react-ui/src/responsive.js
@@ -2,16 +2,16 @@ import { css } from 'styled-components';
 
 export const mobile = (props) => {
     return css`
-        @media only screen and (max-width: 380px) {
+        @media only screen and (max-width: 414px) {
             ${props}
         }
     `;
 };
 
-export const tablet = (props) => {
-    return css`
-        @media only screen and (max-width: 380px) {
-            ${props}
-        }
-    `;
-};
+// export const tablet = (props) => {
+//     return css`
+//         @media only screen and (max-width: 380px) {
+//             ${props}
+//         }
+//     `;
+// };
