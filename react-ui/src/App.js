71a59739476b59843c9f3fd78cf7d621bb437e3f
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-21 16:32:44 +0545

    feat(Routes): Router components added

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,15 +1,18 @@
-import { Home, Cart, ProductList, SingleProduct } from "./pages";
+import { Home, About, Cart, ProductList, SingleProduct } from "./pages";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
     return (
         <Router>
             <div className="App">
-                {/* <Home /> */}
-                <ProductList />
-                {/* <SingleProduct /> */}
-                {/* <Cart /> */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    {/* <Route path="/products" element={<ProductList />} /> */}
+                    {/* <Route path="/product/:id" element={<SingleProduct />} /> */}
+                    {/* <Route path="/cart" element={<Cart />} /> */}
+                </Routes>
             </div>
         </Router>
     );
