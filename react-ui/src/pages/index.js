f73d7721b1a3143f6aea529e03a9d7a254813237
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-21 16:36:17 +0545

    feat(About): About component added

--- react-ui/src/pages/index.js
+++ react-ui/src/pages/index.js
@@ -1,4 +1,5 @@
-export { default as Cart } from "./Cart/Cart";
 export { default as Home } from "./Home/Home";
+export { default as About } from "./About/About";
+export { default as Cart } from "./Cart/Cart";
 export { default as ProductList } from "./ProductList/ProductList";
 export { default as SingleProduct } from "./SingleProduct/SingleProduct";
