1f890ca731da17de698b0f988d74e89b85cbc39c
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-21 15:54:05 +0545

    style(Btn): Btn component set as anchor tag instead of button

--- react-ui/src/components/elements/Button/Button.styles.jsx
+++ react-ui/src/components/elements/Button/Button.styles.jsx
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 
-export const Btn = styled.button`
+export const Btn = styled.a`
     background-color: #fff;
     opacity: 0.5;
-    height: 2.5rem;
-    width: 6rem;
     border-radius: 0.5rem;
+    border: 2px solid grey;
     padding: 0.5rem;
     margin-top: 0.5rem;
     font-weight: bold;
     cursor: pointer;
     :hover {
         background-color: #707070;
+    };
+    :active {
+        scale: 1.025;
     }
 `;
