4e5c61e0422311d3eb003e66b9fd8159525ef13c
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-21 15:57:49 +0545

    feat(react-router-dom): dependency installed

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,14 +1,17 @@
 import { Home, Cart, ProductList, SingleProduct } from "./pages";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
     return (
-        <div className="App">
-            <Home />
-            {/* <ProductList /> */}
-            {/* <SingleProduct /> */}
-            {/* <Cart /> */}
-        </div>
+        <Router>
+            <div className="App">
+                {/* <Home /> */}
+                <ProductList />
+                {/* <SingleProduct /> */}
+                {/* <Cart /> */}
+            </div>
+        </Router>
     );
 };
 
