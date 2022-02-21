e609e54ee96615b447ea30ba22f3168e9d306bca
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-02-21 19:40:12 +0545

    feat(components): respective components exported

--- react-ui/src/pages/index.js
+++ react-ui/src/pages/index.js
@@ -1,5 +1,8 @@
 export { default as Home } from "./Home/Home";
 export { default as About } from "./About/About";
 export { default as Cart } from "./Cart/Cart";
-export { default as ProductList } from "./ProductList/ProductList";
+export { default as Jeans } from "./ProductList/Men/Jeans/Jeans";
+export { default as Jackets } from "./ProductList/Men/Jackets/Jackets";
+export { default as Suits } from "./ProductList/Men/Suits/Suits";
+export { default as Tshirts } from "./ProductList/Men/Tshirts/Tshirts";
 export { default as SingleProduct } from "./SingleProduct/SingleProduct";
