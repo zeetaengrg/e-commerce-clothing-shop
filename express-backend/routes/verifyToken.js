ea85077eb95357ad34abea2f71f01d125e19f34e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-07 21:09:47 +0545

    feat(routes): files added to routes directory

---
+++ express-backend/routes/verifyToken.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token;
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.SECRET_JWT, (err, user) => {
+            if (err) res.status(403).json("Token is not valid!");
+            req.user = user;
+            next();
+        });
+    } else {
+        return res.status(401).json("You are not authenticated");
+    }
+}
+
+const verifyTokenAndAuth = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json("You are not allowed to do that")
+        }
+    })
+}
+
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json("You are not allowed to do that")
+        }
+    })
+}
+
+module.exports = { verifyToken, verifyTokenAndAuth, verifyTokenAndAdmin };
