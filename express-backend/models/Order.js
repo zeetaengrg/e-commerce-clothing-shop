e701d8f40a316f86b7430eb2c7b47cd17d7994b7
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-07 12:45:29 +0545

    feat(schema: order schema created

---
+++ express-backend/models/Order.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema(
+    {
+        userID: { type: String, required: true },
+        products: [
+            {
+                productID: {
+                    type: String,
+                },
+                quantity: {
+                    type: Number,
+                    default: 1,
+                },
+            },
+        ],
+        amount: {
+            type: Number,
+            required: true,
+        },
+        address: {
+            type: Object,
+            required: true,
+        },
+        status: {
+            type: String,
+            default: "pending",
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", CartSchema);
