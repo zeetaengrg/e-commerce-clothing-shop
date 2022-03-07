d6f0a68e7473065ec907c4ce24f35f55f120930f
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-07 12:46:44 +0545

    refactor

--- express-backend/routes/user.js
+++ express-backend/routes/user.js
@@ -1,11 +1,5 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-    res.send("User test successful");
-});
+router.put("/:id", )
 
-router.post("/userposttest", (req, res) => {
-    const username = req.body.username;
-    res.send(`Your Username: ${username}`);
-})
 module.exports = router;
