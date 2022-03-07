10c1c39b01f02ed037a7a7d2482f66200ce819c1
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-07 23:11:42 +0545

    fix(export): OrderSchema export fixed

--- express-backend/models/Order.js
+++ express-backend/models/Order.js
@@ -30,4 +30,4 @@ const OrderSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-module.exports = mongoose.model("Order", CartSchema);
+module.exports = mongoose.model("Order", OrderSchema);
