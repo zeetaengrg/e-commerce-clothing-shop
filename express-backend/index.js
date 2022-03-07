d3375ce9fbcc0097b944a8287b31c946aec1a420
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-07 21:09:04 +0545

    feat(routes): Products routes added

--- express-backend/index.js
+++ express-backend/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
+const productRoutes = require("./routes/product");
 
 // .env file configuration
 dotenv.config();
@@ -22,6 +23,7 @@ app.use(express.json());
 // Create api routes for different routes created
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
 // Create a port number to listen
 app.listen(process.env.PORT || 5500, () => {
