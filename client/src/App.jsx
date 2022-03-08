155909bd71de4ea8bd3f8873fa3a32f82381a301
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-08 16:51:20 +0545

    feat(routes): routing paths updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,10 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about-us" element={<About />} />
-                    <Route path="/male/jeans" element={<Jeans />} />
-                    <Route path="/male/jackets" element={<Jackets />} />
-                    <Route path="/male/tshirts" element={<Tshirts />} />
-                    <Route path="/male/suits" element={<Suits />} />
+                    <Route path="/products/:category" element={<Jeans />} />
+                    {/* <Route path="/products/:category" element={<Jackets />} /> */}
+                    {/* <Route path="/products/:category" element={<Tshirts />} /> */}
+                    {/* <Route path="/male/suits" element={<Suits />} /> */}
                     <Route path="/product/:id" element={<SingleProduct />} />
                     <Route path="/cart" element={<Cart />} />
                 </Routes>
