466472dc81546c6f670e1702ae9b1e035ecd7406
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-08 21:26:18 +0545

    style: styled components styled accordingly

--- client/src/components/modules/Form/SignInOutContainer/SignInOutContainer.styles.jsx
+++ client/src/components/modules/Form/SignInOutContainer/SignInOutContainer.styles.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
-// import { mobile } from '../../../responsive';
 
-export const Box = styled.div`
+export const BoxOne = styled.div`
 
 `;
 
@@ -9,36 +8,63 @@ export const Text = styled.p`
 
 `;
 
-export const Button = styled.button`
+export const Btn = styled.button`
     color: black;
     font-family: inherit;
     font-size: 1rem;
     text-transform: uppercase;
     border-radius: 0.2rem;
     border: 1px solid black;
-    padding: 0 1rem;
+    padding: 0.2rem 0.5rem;
     background-color: white;
     cursor: pointer;
+    transition: all 0.3s ease-in-out;
     &:hover {
         background-color: black;
         color: white;
     };
 `;
 
-export const Modal = styled.div`
+export const ModalDiv = styled.div`
     transition: all 0.3s ease-in-out;
+    display: ${props => props.open ? 'block' : 'none'};
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 999;
+    text-transform: none;
+
 `;
 
-export const Paper = styled.div`
+export const PaperContainer = styled.div`
     width: 340;
     margin: 20px auto;
     background-color: antiquewhite;
+    border-radius: 0.3rem;
 `;
 
-export const TabContainer = styled.div`
-`;
-
-export const Tab = styled.div`
+export const TabBtn = styled.div`
+    background-color: white;
+    border: none;
+    outline: none;
+    padding: 0.8rem 1rem;
     font-family: inherit;
     font-size: 1rem;
+    cursor: pointer;
+    width: 100%;
+    border-radius: 0.3rem 0.3rem 0 0;
+    display: flex;
+    font-size: 1.2rem;
+    text-transform: uppercase;
 `;
+
+export const TabOne = styled.div`
+    flex: 1;
+    text-align: center;
+`;
+
+export const TabTwo = styled.div`
+    flex: 1;
+    text-align: center;
+`
