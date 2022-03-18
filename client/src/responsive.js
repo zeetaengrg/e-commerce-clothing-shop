7a5dc0bcd24210de76968cfd30f226d12b465036
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-18 16:10:56 +0545

    refactor(comment): commented lines removed

--- client/src/responsive.js
+++ client/src/responsive.js
@@ -7,11 +7,3 @@ export const mobile = (props) => {
         }
     `;
 };
-
-// export const tablet = (props) => {
-//     return css`
-//         @media only screen and (max-width: 380px) {
-//             ${props}
-//         }
-//     `;
-// };
