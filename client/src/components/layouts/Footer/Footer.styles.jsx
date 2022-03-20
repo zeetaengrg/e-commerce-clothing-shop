a09ebafb8de6ac7f9c396ba8d4ce4ac7e8c76d66
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-20 13:17:12 +0545

    refactor: styled components deleted

--- client/src/components/layouts/Footer/Footer.styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { mobile } from '../../../responsive';
-
-export const Container = styled.div`
-    display: flex;
-    justify-content: space-between;
-    ${mobile({ flexDirection: "column" })};
-`;
