5f243f55a5a7e8c38d061bffeb038b70dc95b3c8
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-20 13:27:41 +0545

    feat(import): ScrollToTop component imported and added which provides functionality on every page routes to redirect to top of the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,35 @@
-import { Home, About, Cart, Jeans, Jackets, Tshirts, Suits, SingleProduct } from "./pages";
+import {
+  Home,
+  About,
+  Cart,
+  Jeans,
+  Jackets,
+  Tshirts,
+  Suits,
+  SingleProduct,
+} from "./pages";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ScrollToTop } from "./components/elements";
 
 const App = () => {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about-us" element={<About />} />
-                    <Route path="/products/:category" element={<Jeans />} />
-                    {/* <Route path="/products/:category" element={<Jackets />} /> */}
-                    {/* <Route path="/products/:category" element={<Tshirts />} /> */}
-                    {/* <Route path="/male/suits" element={<Suits />} /> */}
-                    <Route path="/product/:id" element={<SingleProduct />} />
-                    <Route path="/cart" element={<Cart />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/products/:category" element={<Jeans />} />
+          {/* <Route path="/products/:category" element={<Jackets />} /> */}
+          {/* <Route path="/products/:category" element={<Tshirts />} /> */}
+          {/* <Route path="/male/suits" element={<Suits />} /> */}
+          <Route path="/product/:id" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
