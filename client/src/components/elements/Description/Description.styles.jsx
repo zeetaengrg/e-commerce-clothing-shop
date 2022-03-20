e1c74f1bc0196a8e36e37858a64e93e452b33a1e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-20 13:14:59 +0545

    refactor: styled components refactored for each elements components

--- client/src/components/elements/Description/Description.styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Desc = styled.p`
-    text-transform: uppercase;
-    background-color: #fff;
-    opacity: 0.5;
-    padding: 0rem 0.5rem;
-    border-radius: 0.5rem;
-`;
