802a487aef2ccfc813897707fcbbab150556b422
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-20 13:23:45 +0545

    feat(hooks): useState and useEffect hooks declared

--- client/src/components/templates/Male/MaleJeans/MaleJeans.jsx
+++ client/src/components/templates/Male/MaleJeans/MaleJeans.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { maleJeans } from '../../../../data';
 import { Products } from '../../../elements'
 import { Container } from './MaleJeans.styles';
+import axios from "axios";
 
-const MaleJeans = () => {
+const MaleJeans = ({ category, filters, sort }) => {
+
+    const [products, setProducts] = useState([]);
+
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async() => {
+            try {
+                const res = await axios.get("http://localhost:5000/api/products");
+                console.log(res);
+            } catch(err) {
+
+            }
+        }
+        getProducts();
+    }, [category]);
+
     return (
         <React.Fragment>
             <Container>
