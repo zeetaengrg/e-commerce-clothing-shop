30c6d2a01512a761ef2bfafa5a98d72a7d079d8f
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-20 13:20:33 +0545

    refactor: styled components deleted

--- client/src/components/modules/Categories/NewArrivalCategory/NewArrivalCategory.styles.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { mobile } from '../../../../responsive';
-
-export const Container = styled.div`
-    display: flex;
-    ${mobile({ flexDirection: "column" })};
-`;
