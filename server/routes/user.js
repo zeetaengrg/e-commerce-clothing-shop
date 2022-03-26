ada891074b53e4a677eabf409bf42430f926dd37
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-26 14:39:48 +0545

    fix(prettier): code prettified

--- server/routes/user.js
+++ server/routes/user.js
@@ -33,12 +33,12 @@ router.put("/:id", verifyTokenAndAuth, async (req, res) => {
 // DELETE
 router.delete("/:id", verifyTokenAndAuth, async (req, res) => {
     try {
-        await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("User has been deleted...")
-    } catch(err) {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+    } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 // GET USER
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
@@ -46,22 +46,22 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
         const user = await User.findById(req.params.id);
         const { password, ...others } = user._doc;
         res.status(200).json(others);
-    } catch(err) {
+    } catch (err) {
         res.status(500).json(err);
-    };
+    }
 });
 
 // GET ALL USER
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
-    const query = req.query.new
+    const query = req.query.new;
     try {
         const users = query
             ? await User.find().sort({ _id: -1 }).limit(5)
             : await User.find();
         res.status(200).json(users);
-    } catch(err) {
+    } catch (err) {
         res.status(500).json(err);
-    };
+    }
 });
 
 // GET USER STATS
@@ -71,23 +71,23 @@ router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
 
     try {
         const data = await User.aggregate([
-            {$match: {createdAt: {$gte: lastYear}}},
+            { $match: { createdAt: { $gte: lastYear } } },
             {
                 $project: {
-                    month: {$month: "$createdAt"}
-                }
+                    month: { $month: "$createdAt" },
+                },
             },
             {
                 $group: {
                     _id: "$month",
                     total: { $sum: 1 },
-                }
-            }
+                },
+            },
         ]);
         res.status(200).json(data);
-    } catch(err) {
+    } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 module.exports = router;
