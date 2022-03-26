ad4dcc0fbf93ccdceea1651b8701967dbb4c2b10
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-26 14:34:34 +0545

    feat(data): updated

--- client/src/data.js
+++ client/src/data.js
@@ -3,32 +3,32 @@ export const slideItems = [
         id: 1,
         img: "https://i.ibb.co/QP3qV5Y/slider1.png",
         title: "WINTER SALE!!!",
-        description: "DON'T MISS OUT ON WINTER SALE. GET FLAT 30% OFF!!!"
+        description: "DON'T MISS OUT ON WINTER SALE. GET FLAT 30% OFF!!!",
     },
     {
         id: 2,
         img: "https://i.ibb.co/rMkkLWD/slider2.jpg",
         title: "SUMMER SALE!!!",
-        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!"
+        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
     },
     {
         id: 3,
         img: "https://i.ibb.co/jGf5b5H/slider3.jpg",
         title: "SUMMER SALE!!!",
-        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!"
+        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
     },
     {
         id: 4,
         img: "https://i.ibb.co/ZMR386v/slider4.jpg",
         title: "SUMMER SALE!!!",
-        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!"
+        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
     },
     {
         id: 5,
         img: "https://i.ibb.co/D1rFrtL/slider5.jpg",
         title: "SUMMER SALE!!!",
-        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!"
-    }
+        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
+    },
 ];
 
 export const categories = [
@@ -36,20 +36,20 @@ export const categories = [
         id: 1,
         img: "https://i.ibb.co/s6P9Vk6/jeans1.png",
         title: "Jeans",
-        category: "jeans"
+        category: "jeans",
     },
     {
         id: 2,
         img: "https://i.ibb.co/BG9Pd4D/jacket1.png",
         title: "Jackets",
-        category: "jackets"
+        category: "jackets",
     },
     {
         id: 3,
         img: "https://i.ibb.co/Zh200R9/tee1.png",
         title: "T-shirts",
-        category: "tshirts"
-    }
+        category: "tshirts",
+    },
 ];
 
 export const newArrivalsItems = [
@@ -57,142 +57,138 @@ export const newArrivalsItems = [
         id: 1,
         img: "https://i.ibb.co/7yWMzHr/mennewarrival.png",
         title: "MEN'S NEW ARRIVALS!!!",
-        description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!"
+        description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!",
     },
     {
         id: 2,
         img: "https://i.ibb.co/mGsNc9r/womennewarrival.jpg",
         title: "WOMEN'S NEW ARRIVALS!!!",
-        description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!"
-    }
+        description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!",
+    },
 ];
 
 export const maleJeans = [
     {
         id: 1,
-        img: "https://i.ibb.co/s6P9Vk6/jeans1.png"
+        img: "https://i.ibb.co/s6P9Vk6/jeans1.png",
     },
     {
         id: 2,
-        img: "https://i.ibb.co/k0WQyFN/jeans2.png"
+        img: "https://i.ibb.co/k0WQyFN/jeans2.png",
     },
     {
         id: 3,
-        img: "https://i.ibb.co/pJ35J9K/jeans3.png"
+        img: "https://i.ibb.co/pJ35J9K/jeans3.png",
     },
     {
         id: 4,
-        img: "https://i.ibb.co/zPm5Qqc/jeans4.png"
+        img: "https://i.ibb.co/zPm5Qqc/jeans4.png",
     },
     {
         id: 5,
-        img: "https://i.ibb.co/WV7RCKx/jeans5.png"
+        img: "https://i.ibb.co/WV7RCKx/jeans5.png",
     },
     {
         id: 6,
-        img: "https://i.ibb.co/qrs3b0d/jeans6.png"
+        img: "https://i.ibb.co/qrs3b0d/jeans6.png",
     },
     {
         id: 7,
-        img: "https://i.ibb.co/T4L1SVV/jeans7.png"
+        img: "https://i.ibb.co/T4L1SVV/jeans7.png",
     },
     {
         id: 8,
-        img: "https://i.ibb.co/1n5d8zM/jeans8.png"
+        img: "https://i.ibb.co/1n5d8zM/jeans8.png",
     },
-    {
-        id: 9,
-        img: "https://i.ibb.co/cyNkqSg/jeans9.png"
-    }
 ];
 
 export const maleJackets = [
     {
         id: 1,
-        img: "https://i.ibb.co/BG9Pd4D/jacket1.png"
+        img: "https://i.ibb.co/BG9Pd4D/jacket1.png",
     },
     {
         id: 2,
-        img: "https://i.ibb.co/cwYmBf8/jacket2.png"
+        img: "https://i.ibb.co/cwYmBf8/jacket2.png",
     },
     {
         id: 3,
-        img: "https://i.ibb.co/nbpP4SR/jacket3.png"
+        img: "https://i.ibb.co/nbpP4SR/jacket3.png",
     },
     {
         id: 4,
-        img: "https://i.ibb.co/PFjnFB5/jacket4.png"
+        img: "https://i.ibb.co/PFjnFB5/jacket4.png",
     },
     {
         id: 5,
-        img: "https://i.ibb.co/37q1mt5/jacket13.webp"
+        img: "https://i.ibb.co/37q1mt5/jacket13.webp",
     },
     {
         id: 6,
-        img: "https://i.ibb.co/j3hWCDH/jacket4.webp"
-    }
+        img: "https://i.ibb.co/j3hWCDH/jacket4.webp",
+    },
 ];
 
 export const maleSuits = [
     {
         id: 1,
-        img: "https://i.ibb.co/rwKCtct/suit1.png"
+        img: "https://i.ibb.co/rwKCtct/suit1.png",
     },
     {
         id: 2,
-        img: "https://i.ibb.co/3z6pD9k/suit2.png"
+        img: "https://i.ibb.co/3z6pD9k/suit2.png",
     },
     {
         id: 3,
-        img: "https://i.ibb.co/ynQHcmZ/suit3.png"
+        img: "https://i.ibb.co/ynQHcmZ/suit3.png",
     },
     {
         id: 4,
-        img: "https://i.ibb.co/ts5Zt9z/suit4.png"
+        img: "https://i.ibb.co/ts5Zt9z/suit4.png",
     },
     {
         id: 5,
-        img: "https://i.ibb.co/Y3BSG4g/suit5.png"
+        img: "https://i.ibb.co/Y3BSG4g/suit5.png",
     },
     {
         id: 6,
-        img: "https://i.ibb.co/gtgkPSG/suit6.png"
+        img: "https://i.ibb.co/gtgkPSG/suit6.png",
     },
     {
         id: 7,
-        img: "https://i.ibb.co/kJcKdzY/suit7.png"
-    }
+        img: "https://i.ibb.co/kJcKdzY/suit7.png",
+    },
 ];
 
 export const femalePants = [
     {
         id: 1,
-        img: "https://i.ibb.co/1sd7kNB/pants1.jpg"
-    }
+        img: "https://i.ibb.co/1sd7kNB/pants1.jpg",
+    },
 ];
 
 export const maleTShirts = [
     {
         id: 1,
-        img: "https://i.ibb.co/Zh200R9/tee1.png"
+        img: "https://i.ibb.co/Zh200R9/tee1.png",
     },
     {
         id: 2,
-        img: "https://i.ibb.co/txBqbfM/tee2.png"
+        img: "https://i.ibb.co/txBqbfM/tee2.png",
     },
     {
         id: 3,
-        img: "https://i.ibb.co/d4JC6GC/tee3.png"
+        img: "https://i.ibb.co/d4JC6GC/tee3.png",
     },
     {
         id: 4,
-        img: "https://i.ibb.co/Bzs017y/tee4.png"
-    }
+        img: "https://i.ibb.co/Bzs017y/tee4.png",
+    },
 ];
 
 export const femaleTShirts = [
     {
         id: 1,
-        img: ""
-    }
+        img: "",
+    },
 ];
