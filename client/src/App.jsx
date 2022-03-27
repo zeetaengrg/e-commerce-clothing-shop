cfb8baea8a7b0b50c3c8f41991ddbd9e3a6bd8cf
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-27 22:18:44 +0545

    feat(routes): new routes to product list page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,26 @@
-import {
-  Home,
-  About,
-  Cart,
-  Jeans,
-  Jackets,
-  Tshirts,
-  Suits,
-  SingleProduct,
-} from "./pages";
+import { Home, About, Cart, ProductList, SingleProduct } from "./pages";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ScrollToTop } from "./components/elements";
 
 const App = () => {
-  return (
-    <Router>
-      <ScrollToTop />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<About />} />
-          <Route path="/products/:category" element={<Jeans />} />
-          {/* <Route path="/products/:category" element={<Jackets />} /> */}
-          {/* <Route path="/products/:category" element={<Tshirts />} /> */}
-          {/* <Route path="/male/suits" element={<Suits />} /> */}
-          <Route path="/product/:id" element={<SingleProduct />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <ScrollToTop />
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about-us" element={<About />} />
+                    <Route
+                        path="/products/:category"
+                        element={<ProductList />}
+                    />
+                    <Route path="/product/:id" element={<SingleProduct />} />
+                    <Route path="/cart" element={<Cart />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
