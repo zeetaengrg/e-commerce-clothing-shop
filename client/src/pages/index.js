dccdcc70b9ee5a821fb683cd0a6cf20c69fdb466
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-27 22:22:17 +0545

    feat(export): ProductList component exported

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,5 @@
 export { default as Home } from "./Home/Home";
 export { default as About } from "./About/About";
 export { default as Cart } from "./Cart/Cart";
-export { default as Jeans } from "./ProductList/Jeans/Jeans";
-export { default as Jackets } from "./ProductList/Jackets/Jackets";
-export { default as Suits } from "./ProductList/Suits/Suits";
-export { default as Tshirts } from "./ProductList/Tshirts/Tshirts";
+export { default as ProductList } from "./ProductList/ProductList";
 export { default as SingleProduct } from "./SingleProduct/SingleProduct";
