41fe184c1d3dc587b1da69a108c28b8ce8558553
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-28 12:34:07 +0545

    refactor(prettier)

--- client/src/components/layouts/Hoodies/Hoodies.jsx
+++ client/src/components/layouts/Hoodies/Hoodies.jsx
@@ -4,15 +4,15 @@ import { Products } from "../../elements";
 import { Container } from "./Hoodies.styles";
 
 const Hoodies = ({ category, filters, sort }) => {
-    return (
-        <React.Fragment>
-            <Container>
-                {hoodiesWear.map((product) => (
-                    <Products key={product.id} product={product} />
-                ))}
-            </Container>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Container>
+        {hoodiesWear.map((product) => (
+          <Products key={product.id} product={product} />
+        ))}
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default Hoodies;
