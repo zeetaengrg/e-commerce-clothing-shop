150b1df980d3f8689752d864d52f2538e2ddaa4b
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-29 15:17:55 +0545

    style(margin and padding): AddToCart button margin and padding modified

--- client/src/pages/SingleProduct/SingleProduct.styles.js
+++ client/src/pages/SingleProduct/SingleProduct.styles.js
@@ -4,163 +4,182 @@ import { mobile } from "../../responsive";
 export const Container = styled.div``;
 
 export const Wrapper = styled.div`
-    display: flex;
-    ${mobile({ flexDirection: "column" })};
+  display: flex;
+  ${mobile({ flexDirection: "column" })};
 `;
 
 export const ImageContainer = styled.div`
-    flex: 1;
-    margin: 1rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #f5f5f5;
-    position: relative;
+  flex: 1;
+  margin: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5f5f5;
+  position: relative;
+  height: 550px;
+  border-radius: 0.5rem;
 `;
 
 export const Circle = styled.div`
-    height: 400px;
-    width: 400px;
-    border-radius: 50%;
-    background-color: white;
-    position: absolute;
-    ${mobile({ height: "350px", width: "350px" })};
+  height: 450px;
+  width: 450px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+  ${mobile({ height: "350px", width: "350px" })};
 `;
 
 export const Image = styled.img`
-    z-index: 2;
-    ${mobile({ height: "400px" })};
+  z-index: 2;
+  height: 85%;
+  ${mobile({ height: "400px" })};
 `;
 
 export const Info = styled.div`
-    flex: 1;
-    background-color: #f5f5f5;
-    margin: 1rem;
-    padding: 0.5rem 1rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  background-color: #f5f5f5;
+  margin: 1rem;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
 `;
 
-export const Title = styled.h2`
-    font-size: 2rem;
-    font-weight: 400;
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 400;
+  line-height: 1.6;
 `;
 
 export const Divider = styled.hr`
-    width: 100%;
-    opacity: 0.2;
+  width: 100%;
+  opacity: 0.2;
 `;
 
 export const Description = styled.p`
-    margin-top: 1.5rem;
-    margin-bottom: 1.5rem;
-    width: 85%;
+  width: 85%;
+  font-size: 1.2rem;
+  font-weight: 300;
+`;
+
+export const ReviewContainer = styled.div``;
+
+export const TotalReviews = styled.span`
+  font-size: 1.2rem;
+  font-weight: 400;
 `;
 
 export const Price = styled.span`
-    font-size: 2.5rem;
-    font-weight: 300;
+  font-size: 2rem;
+  font-weight: 300;
 `;
 
+export const Stock = styled.p`
+  font-size: 1rem;
+  font-weight: 300;
+`;
+
+export const StockTitle = styled(TotalReviews)``;
+
 export const SortContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-top: 1.5rem;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
 `;
 
 export const ColorContainer = styled.div`
-    display: flex;
-    margin-right: 1rem;
+  display: flex;
+  gap: 0.5rem;
 `;
 
-export const ColorText = styled.span`
-    margin-right: 0.5rem;
-`;
+export const ColorText = styled(StockTitle)``;
 
 export const ColorInfo = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
 `;
 
 export const Color = styled.div`
-    margin-right: 0.5rem;
-    width: 1rem;
-    height: 1rem;
-    border-radius: 50%;
-    border: 0.2px solid grey;
-    background-color: ${(props) => props.color};
-    cursor: pointer;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  border: 0.2px solid grey;
+  background-color: ${(props) => props.color};
+  cursor: pointer;
 `;
 
 export const SizeContainer = styled.div`
-    display: flex;
+  display: flex;
+  gap: 0.5rem;
 `;
 
-export const SizeText = styled.span`
-    margin-right: 0.5rem;
-`;
+export const SizeText = styled(StockTitle)``;
 
 export const SizeInfo = styled.div``;
 
 export const Select = styled.select`
-    background: transparent;
-    cursor: pointer;
-    border: 1px solid black;
-    border-radius: 0.2rem;
+  background: transparent;
+  cursor: pointer;
+  border: 1px solid black;
+  border-radius: 0.2rem;
 `;
 
 export const Options = styled.option`
-    padding: 0.2rem;
+  padding: 0.2rem;
 `;
 
 export const CartInfo = styled.div`
-    display: flex;
-    align-items: center;
-    margin-top: 1.5rem;
-    gap: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  margin-top: 1rem;
 `;
 
 export const CounterBtn = styled.div`
-    display: flex;
+  display: flex;
+  justify-content: space-around;
+  gap: 1rem;
+  width: 10rem;
 `;
 
 export const MinusBtn = styled.button`
-    border: none;
-    background-color: transparent;
-    font-size: 1.5rem;
-    font-weight: bold;
-    cursor: pointer;
-    margin-right: 0.2rem;
-    display: grid;
-    place-items: center;
+  border: none;
+  background-color: transparent;
+  font-size: 1.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  display: grid;
+  place-items: center;
 `;
 
 export const Counter = styled.span`
-    font-size: 1rem;
-    border: 1px solid black;
-    padding: 0 0.5rem;
-    margin-right: 0.2rem;
-    border-radius: 0.3rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  font-size: 1.2rem;
+  border: 1px solid black;
+  padding: 0 1rem;
+  border-radius: 0.3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const AddBtn = styled(MinusBtn)`
-    margin-right: 0rem;
-`;
+export const AddBtn = styled(MinusBtn)``;
 
 export const AddToCart = styled.button`
-    padding: 0.5rem 1rem;
-    font-size: 1rem;
-    text-transform: uppercase;
-    background: transparent;
-    border: 1px solid black;
-    color: black;
-    border-radius: 0.3rem;
-    cursor: pointer;
-    font-family: inherit;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  width: 10rem;
+  text-transform: uppercase;
+  background: transparent;
+  border: 1px solid black;
+  color: black;
+  border-radius: 0.3rem;
+  cursor: pointer;
+  font-family: inherit;
 
-    &:hover {
-        background: black;
-        color: white;
-    }
+  &:hover {
+    background: black;
+    color: white;
+  }
 `;
