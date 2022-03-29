17c192e904abbc9f540f2e662fdc53463454e8cc
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-29 15:15:12 +0545

    fix(prettier): code prettified

--- client/src/components/layouts/Jeans/Jeans.jsx
+++ client/src/components/layouts/Jeans/Jeans.jsx
@@ -4,63 +4,63 @@ import { Container } from "./Jeans.styles";
 import axios from "axios";
 
 const Jeans = ({ category, filters, sort }) => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    const [filteredProducts, setFilteredProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-    useEffect(() => {
-        const getProducts = async () => {
-            try {
-                const res = await axios.get(
-                    category
-                        ? `http://localhost:5000/api/products?category=${category}`
-                        : `http://localhost:5000/api/products`
-                );
-                setProducts(res.data);
-            } catch (err) {}
-        };
-        getProducts();
-    }, [category]);
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          category
+            ? `http://localhost:5000/api/products?category=${category}`
+            : `http://localhost:5000/api/products`
+        );
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [category]);
 
-    useEffect(() => {
-        category &&
-            setFilteredProducts(
-                products.filter((product) =>
-                    Object.entries(filters).every(([key, value]) =>
-                        product[key].includes(value)
-                    )
-                )
-            );
-    }, [category, filters, products]);
+  useEffect(() => {
+    category &&
+      setFilteredProducts(
+        products.filter((product) =>
+          Object.entries(filters).every(([key, value]) =>
+            product[key].includes(value)
+          )
+        )
+      );
+  }, [category, filters, products]);
 
-    useEffect(() => {
-        const sortProducts = () => {
-            if (sort === "newest") {
-                setFilteredProducts((prev) =>
-                    [...prev].sort((a, b) => a.createdAt - b.createdAt)
-                );
-            } else if (sort === "lowToHigh") {
-                setFilteredProducts((prev) =>
-                    [...prev].sort((a, b) => a.price - b.price)
-                );
-            } else {
-                setFilteredProducts((prev) =>
-                    [...prev].sort((a, b) => b.price - a.price)
-                );
-            }
-        };
-        sortProducts();
-    }, [sort]);
+  useEffect(() => {
+    const sortProducts = () => {
+      if (sort === "newest") {
+        setFilteredProducts((prev) =>
+          [...prev].sort((a, b) => a.createdAt - b.createdAt)
+        );
+      } else if (sort === "lowToHigh") {
+        setFilteredProducts((prev) =>
+          [...prev].sort((a, b) => a.price - b.price)
+        );
+      } else {
+        setFilteredProducts((prev) =>
+          [...prev].sort((a, b) => b.price - a.price)
+        );
+      }
+    };
+    sortProducts();
+  }, [sort]);
 
-    return (
-        <React.Fragment>
-            <Container>
-                {filteredProducts.map((product) => (
-                    <Products key={product.id} product={product} />
-                ))}
-            </Container>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Container>
+        {filteredProducts.map((product) => (
+          <Products key={product.id} product={product} />
+        ))}
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default Jeans;
