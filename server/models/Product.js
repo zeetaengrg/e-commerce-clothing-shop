62b4b7957cd71c0a3acd063973ff80f462c8b2fa
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-29 15:18:29 +0545

    feat(schema): new reviews and ratings schema added

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
-    {
-        title: { type: String, required: true, unique: true },
-        description: { type: String, required: true },
-        img: { type: String, required: true },
-        categories: { type: Array },
-        size: { type: Array },
-        color: { type: Array },
-        price: { type: Number, required: true },
-        brand: { type: String, required: true },
-        inStock: { type: Boolean, default: true },
-    },
-    { timestamps: true }
+  {
+    title: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    img: { type: String, required: true },
+    categories: { type: Array },
+    size: { type: Array },
+    color: { type: Array },
+    price: { type: Number, required: true },
+    brand: { type: String, required: true },
+    inStock: { type: Boolean, default: true },
+    reviews: { type: Number, required: true },
+    ratings: { type: Number, required: true },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Product", ProductSchema);
