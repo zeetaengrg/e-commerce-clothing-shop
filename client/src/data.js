11da279d7ae6161eb83f11cc2a473f8cbef22c4c
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-29 15:27:51 +0545

    fix(prettier): code prettified

--- client/src/data.js
+++ client/src/data.js
@@ -1,234 +1,234 @@
 export const slideItems = [
-    {
-        id: 1,
-        img: "https://i.ibb.co/QP3qV5Y/slider1.png",
-        title: "WINTER SALE!!!",
-        description: "DON'T MISS OUT ON WINTER SALE. GET FLAT 30% OFF!!!",
-    },
-    {
-        id: 2,
-        img: "https://i.ibb.co/rMkkLWD/slider2.jpg",
-        title: "SUMMER SALE!!!",
-        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
-    },
-    {
-        id: 3,
-        img: "https://i.ibb.co/ZMR386v/slider4.jpg",
-        title: "SUMMER SALE!!!",
-        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
-    },
-    {
-        id: 4,
-        img: "https://i.ibb.co/D1rFrtL/slider5.jpg",
-        title: "SUMMER SALE!!!",
-        description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
-    },
+  {
+    id: 1,
+    img: "https://i.ibb.co/QP3qV5Y/slider1.png",
+    title: "WINTER SALE!!!",
+    description: "DON'T MISS OUT ON WINTER SALE. GET FLAT 30% OFF!!!",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/rMkkLWD/slider2.jpg",
+    title: "SUMMER SALE!!!",
+    description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/ZMR386v/slider4.jpg",
+    title: "SUMMER SALE!!!",
+    description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/D1rFrtL/slider5.jpg",
+    title: "SUMMER SALE!!!",
+    description: "DON'T MISS OUT ON SUMMER SALE. GET FLAT 30% OFF!!!",
+  },
 ];
 
 export const categories = [
-    {
-        id: 1,
-        img: "https://i.ibb.co/s6P9Vk6/jeans1.png",
-        title: "Jeans",
-        category: "jeans",
-    },
-    {
-        id: 2,
-        img: "https://i.ibb.co/BG9Pd4D/jacket1.png",
-        title: "Jackets",
-        category: "jackets",
-    },
-    {
-        id: 3,
-        img: "https://i.ibb.co/Zh200R9/tee1.png",
-        title: "T-shirts",
-        category: "tshirts",
-    },
-    {
-        id: 4,
-        img: "https://i.ibb.co/HFsbpDk/hoody3.png",
-        title: "Hoodies",
-        category: "hoodies",
-    },
-    {
-        id: 5,
-        img: "https://i.ibb.co/rwKCtct/suit1.png",
-        title: "Suits",
-        category: "suits",
-    },
-    {
-        id: 6,
-        img: "https://i.ibb.co/RczhjhR/shoes1.png",
-        title: "Shoes",
-        category: "shoes",
-    },
+  {
+    id: 1,
+    img: "https://i.ibb.co/s6P9Vk6/jeans1.png",
+    title: "Jeans",
+    category: "jeans",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/BG9Pd4D/jacket1.png",
+    title: "Jackets",
+    category: "jackets",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/Zh200R9/tee1.png",
+    title: "T-shirts",
+    category: "tshirts",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/HFsbpDk/hoody3.png",
+    title: "Hoodies",
+    category: "hoodies",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/rwKCtct/suit1.png",
+    title: "Suits",
+    category: "suits",
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/RczhjhR/shoes1.png",
+    title: "Shoes",
+    category: "shoes",
+  },
 ];
 
 export const newArrivalsItems = [
-    {
-        id: 1,
-        img: "https://i.ibb.co/7yWMzHr/mennewarrival.png",
-        title: "NEW WINTER ARRIVALS!!!",
-        description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!",
-    },
-    {
-        id: 2,
-        img: "https://i.ibb.co/mGsNc9r/womennewarrival.jpg",
-        title: "NEW SPRING ARRIVALS!!!",
-        description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!",
-    },
+  {
+    id: 1,
+    img: "https://i.ibb.co/7yWMzHr/mennewarrival.png",
+    title: "NEW WINTER ARRIVALS!!!",
+    description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/mGsNc9r/womennewarrival.jpg",
+    title: "NEW SPRING ARRIVALS!!!",
+    description: "NEW ARRIVALS IN THE STORE. GET THEM TODAY!!!",
+  },
 ];
 
 export const jeansWear = [
-    {
-        id: 1,
-        img: "https://i.ibb.co/s6P9Vk6/jeans1.png",
-    },
-    {
-        id: 2,
-        img: "https://i.ibb.co/k0WQyFN/jeans2.png",
-    },
-    {
-        id: 3,
-        img: "https://i.ibb.co/pJ35J9K/jeans3.png",
-    },
-    {
-        id: 4,
-        img: "https://i.ibb.co/zPm5Qqc/jeans4.png",
-    },
-    {
-        id: 5,
-        img: "https://i.ibb.co/WV7RCKx/jeans5.png",
-    },
-    {
-        id: 6,
-        img: "https://i.ibb.co/qrs3b0d/jeans6.png",
-    },
-    {
-        id: 7,
-        img: "https://i.ibb.co/T4L1SVV/jeans7.png",
-    },
-    {
-        id: 8,
-        img: "https://i.ibb.co/1n5d8zM/jeans8.png",
-    },
+  {
+    id: 1,
+    img: "https://i.ibb.co/s6P9Vk6/jeans1.png",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/k0WQyFN/jeans2.png",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/pJ35J9K/jeans3.png",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/zPm5Qqc/jeans4.png",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/WV7RCKx/jeans5.png",
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/qrs3b0d/jeans6.png",
+  },
+  {
+    id: 7,
+    img: "https://i.ibb.co/T4L1SVV/jeans7.png",
+  },
+  {
+    id: 8,
+    img: "https://i.ibb.co/1n5d8zM/jeans8.png",
+  },
 ];
 
 export const jacketsWear = [
-    {
-        id: 1,
-        img: "https://i.ibb.co/BG9Pd4D/jacket1.png",
-    },
-    {
-        id: 2,
-        img: "https://i.ibb.co/cwYmBf8/jacket2.png",
-    },
-    {
-        id: 3,
-        img: "https://i.ibb.co/nbpP4SR/jacket3.png",
-    },
-    {
-        id: 4,
-        img: "https://i.ibb.co/PFjnFB5/jacket4.png",
-    },
-    {
-        id: 5,
-        img: "https://i.ibb.co/37q1mt5/jacket13.webp",
-    },
-    {
-        id: 6,
-        img: "https://i.ibb.co/j3hWCDH/jacket4.webp",
-    },
+  {
+    id: 1,
+    img: "https://i.ibb.co/BG9Pd4D/jacket1.png",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/cwYmBf8/jacket2.png",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/nbpP4SR/jacket3.png",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/PFjnFB5/jacket4.png",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/37q1mt5/jacket13.webp",
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/j3hWCDH/jacket4.webp",
+  },
 ];
 
 export const suitsWear = [
-    {
-        id: 1,
-        img: "https://i.ibb.co/rwKCtct/suit1.png",
-    },
-    {
-        id: 2,
-        img: "https://i.ibb.co/3z6pD9k/suit2.png",
-    },
-    {
-        id: 3,
-        img: "https://i.ibb.co/ynQHcmZ/suit3.png",
-    },
-    {
-        id: 4,
-        img: "https://i.ibb.co/ts5Zt9z/suit4.png",
-    },
-    {
-        id: 5,
-        img: "https://i.ibb.co/Y3BSG4g/suit5.png",
-    },
-    {
-        id: 6,
-        img: "https://i.ibb.co/gtgkPSG/suit6.png",
-    },
-    {
-        id: 7,
-        img: "https://i.ibb.co/kJcKdzY/suit7.png",
-    },
+  {
+    id: 1,
+    img: "https://i.ibb.co/rwKCtct/suit1.png",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/3z6pD9k/suit2.png",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/ynQHcmZ/suit3.png",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/ts5Zt9z/suit4.png",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/Y3BSG4g/suit5.png",
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/gtgkPSG/suit6.png",
+  },
+  {
+    id: 7,
+    img: "https://i.ibb.co/kJcKdzY/suit7.png",
+  },
 ];
 
 export const shirtsWear = [
-    {
-        id: 1,
-        img: "https://i.ibb.co/Zh200R9/tee1.png",
-    },
-    {
-        id: 2,
-        img: "https://i.ibb.co/txBqbfM/tee2.png",
-    },
-    {
-        id: 3,
-        img: "https://i.ibb.co/d4JC6GC/tee3.png",
-    },
-    {
-        id: 4,
-        img: "https://i.ibb.co/Bzs017y/tee4.png",
-    },
+  {
+    id: 1,
+    img: "https://i.ibb.co/Zh200R9/tee1.png",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/txBqbfM/tee2.png",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/d4JC6GC/tee3.png",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/Bzs017y/tee4.png",
+  },
 ];
 
 export const hoodiesWear = [
-    {
-        id: 1,
-        img: "https://i.ibb.co/vB4yRLx/hoody2.png",
-    },
-    {
-        id: 2,
-        img: "https://i.ibb.co/HFsbpDk/hoody3.png",
-    },
-    {
-        id: 3,
-        img: "https://i.ibb.co/t81GkC0/hoody4.png",
-    },
-    {
-        id: 4,
-        img: "https://i.ibb.co/XX7bTzw/hoody5.png",
-    },
+  {
+    id: 1,
+    img: "https://i.ibb.co/vB4yRLx/hoody2.png",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/HFsbpDk/hoody3.png",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/t81GkC0/hoody4.png",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/XX7bTzw/hoody5.png",
+  },
 ];
 
 export const shoesWear = [
-    {
-        id: 1,
-        img: "https://i.ibb.co/RczhjhR/shoes1.png",
-    },
-    {
-        id: 2,
-        img: "https://i.ibb.co/ss6WdkJ/shoes2.png",
-    },
-    {
-        id: 3,
-        img: "https://i.ibb.co/R6pQHqJ/shoes3.png",
-    },
-    {
-        id: 4,
-        img: "https://i.ibb.co/pfzxmwR/shoes4.png",
-    },
-    {
-        id: 5,
-        img: "https://i.ibb.co/RD6N18h/shoes5.png",
-    },
+  {
+    id: 1,
+    img: "https://i.ibb.co/RczhjhR/shoes1.png",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/ss6WdkJ/shoes2.png",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/R6pQHqJ/shoes3.png",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/pfzxmwR/shoes4.png",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/RD6N18h/shoes5.png",
+  },
 ];
