51e913eb02a7b79c91d6df0f4c64f12c260e4ff3
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-29 18:33:46 +0545

    refactor(prettier): code prettified

--- client/src/pages/ProductList/ProductList.styles.js
+++ client/src/pages/ProductList/ProductList.styles.js
@@ -1,33 +1,27 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div`
-
-`;
+export const Container = styled.div``;
 
 export const Title = styled.h1`
-    padding: 1rem;
-    text-transform: uppercase;
-    font-weight: 400;
+  padding: 1rem;
+  text-transform: uppercase;
+  font-weight: 400;
 `;
 
 export const FilterContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
 `;
 
-export const Filter = styled.div`
-
-`;
+export const Filter = styled.div``;
 
 export const FilterText = styled.span`
-    margin-right: 1rem;
+  margin-right: 1rem;
 `;
 
 export const Select = styled.select`
-    margin-right: 1rem;
+  margin-right: 1rem;
 `;
 
-export const Options = styled.option`
-
-`;
+export const Options = styled.option``;
