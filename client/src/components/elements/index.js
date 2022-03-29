097f4fcbf852e02da0c663a88fbdcf0e102a80b8
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-29 15:14:54 +0545

    feat(expor): Ratings component exported

--- client/src/components/elements/index.js
+++ client/src/components/elements/index.js
@@ -4,4 +4,5 @@ export { default as Button } from "./Button/Button";
 export { default as Description } from "./Description/Description";
 export { default as Header } from "./Header/Header";
 export { default as Products } from "./Products/Products";
+export { default as Ratings } from "./Ratings/Ratings";
 export { default as ScrollToTop } from "./ScrollToTop/ScrollToTop";
