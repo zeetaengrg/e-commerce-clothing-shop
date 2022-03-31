8c9df1434a4d84ef1e7d27f15e1ec627bfd9ae15
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-31 14:31:05 +0545

    style(color): color of Container modified to teal

--- client/src/components/elements/Ratings/Ratings.styles.js
+++ client/src/components/elements/Ratings/Ratings.styles.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  color: red;
+export const Container = styled.section`
+  color: teal;
   font-size: 1.2rem;
 `;
 
-export const RatingStar = styled.span`
-  cursor: pointer;
-`;
+export const RatingStar = styled.span``;
