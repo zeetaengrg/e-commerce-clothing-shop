7fd83bf7214e550ef740f9aaccea6a99b11bab3e
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-03-31 12:58:42 +0545

    feat(hooks): useState and useSelecteor hooks from react and react-redux imported

--- client/src/components/templates/Navbar/Navbar.jsx
+++ client/src/components/templates/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import SignInOutContainer from "../../modules/Form/SignInOutContainer/SignInOutContainer";
