82f83c10468a49872ead740381fbf145c727aa5d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-05 13:37:57 +0545

    feat(refactor): react 18 modules and components used after upgrading to react 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+
+import App from "./App";
 import store from "./redux/store";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
