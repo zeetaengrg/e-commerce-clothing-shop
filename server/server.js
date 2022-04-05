98ecb913c44e1fdead63679a565124631c39e898
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-05 16:17:07 +0545

    feat(package): cors added

--- server/server.js
+++ server/server.js
@@ -1,27 +1,35 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
+
 const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
 const orderRoutes = require("./routes/order");
 
+const app = express();
+const port = process.env.PORT || 5000;
+
 // .env file configuration
 dotenv.config();
 
 // Connecting the MongoDB database
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log("Connected to database!")
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
+
+// Cors Middleware
+app.use(cors());
+
 // Create api routes for different routes created
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -30,6 +38,6 @@ app.use("/api/carts", cartRoutes);
 app.use("/api/orders", orderRoutes);
 
 // Create a port number to listen
-app.listen(process.env.PORT || 5500, () => {
-    console.log(`Backend server is running on port ${process.env.PORT}`);
+app.listen(port || 5500, () => {
+  console.log(`Backend server is running on port ${process.env.PORT}`);
 });
