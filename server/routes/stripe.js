3cf2c9e3027a05c8c7a1a85a1fb8c3ea831ef5c9
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-06 14:24:00 +0545

    feat(environment variables): new variables created and used accordingly

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(process.env.REACT_APP_STRIPE_KEY);
 
 router.post("/payment", (req, res) => {
-    stripe.charges.create(
-        {
-            source: req.body.tokenId,
-            amount: req.body.amount,
-            currency: "USD",
-        },
-        (stripeErr, stripeRes) => {
-            if(stripeErr) {
-                res.status(500).json(stripeErr);
-            } else {
-                res.status(200).json(stripeRes);
-            }
-        }
-    );
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "USD",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
 });
 
-module.exports = router; 
+module.exports = router;
