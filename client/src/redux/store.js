83febbc12e8958e611f9a9ae7a760fda67531fb2
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-11 14:50:50 +0545

    feat(reducer added): user reducer added to the store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
+import userReducer from "./userRedux";
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
+    user: userReducer,
   },
 });
