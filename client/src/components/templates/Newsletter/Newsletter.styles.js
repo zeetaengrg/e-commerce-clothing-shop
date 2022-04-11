008dc7dac20338206b5eb5db89d90f6701e8f40b
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-11 23:16:50 +0545

    feat(button): button color updated

--- client/src/components/templates/Newsletter/Newsletter.styles.js
+++ client/src/components/templates/Newsletter/Newsletter.styles.js
@@ -1,48 +1,54 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    display: grid;
-    place-content: center;
-    height: 50vh;
-    background-color: #f5f5f5;
+  display: grid;
+  place-content: center;
+  height: 50vh;
+  background-color: #f5f5f5;
 `;
 
 export const Title = styled.h1`
-    text-align: center;
-    font-size: 3rem;
+  text-align: center;
+  font-size: 3rem;
 `;
 
 export const Description = styled.p`
-    text-align: center;
-    margin-bottom: 1rem;
+  text-align: center;
+  margin-bottom: 1rem;
 `;
 
 export const Form = styled.div`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 export const Input = styled.input`
-    width: 30vw;
-    border: 1px solid #707070;
-    border-radius: 0.2rem;
-    border-bottom-right-radius: 0;
-    border-top-right-radius: 0;
-    padding: 0.5rem;
-    font: inherit;
+  width: 30vw;
+  border: 1px solid grey;
+  border-radius: 0.2rem;
+  border-bottom-right-radius: 0;
+  border-top-right-radius: 0;
+  padding: 0.5rem;
+  font: inherit;
 `;
 
 export const Button = styled.button`
-    cursor: pointer;
-    background-color: teal;
-    border: none;
-    border-radius: 0.2rem;
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-    width: 3rem;
-    display: grid;
-    place-content: center;
-    :hover {
-        color: white;
-    }
+  cursor: pointer;
+  background-color: black;
+  border: 1px solid grey;
+  border-radius: 0.2rem;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  width: 3rem;
+  display: grid;
+  place-content: center;
+  color: white;
+  transition: all 0.3s ease;
+
+  :hover {
+    color: black;
+    background-color: white;
+    border-top-right-radius: 0.2rem;
+    border-bottom-right-radius: 0.2rem;
+  }
 `;
