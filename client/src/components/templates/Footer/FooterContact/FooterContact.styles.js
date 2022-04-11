4c3436c2eda11199e022608ba1ea6561a2899a35
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-11 23:16:14 +0545

    refactor

--- client/src/components/templates/Footer/FooterContact/FooterContact.styles.js
+++ client/src/components/templates/Footer/FooterContact/FooterContact.styles.js
@@ -1,39 +1,39 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    flex: 1;
-    margin: 1rem;
+  flex: 1;
+  margin: 1rem;
 `;
 
 export const Title = styled.h3`
-    margin-bottom: 1rem;
+  margin-bottom: 1rem;
 `;
 
 export const Location = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
 `;
 
 export const PhoneNumber = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
 `;
 
 export const Email = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
 `;
 
 export const CreditCards = styled.div`
-    display: flex;
-    gap: 1rem;
+  display: flex;
+  gap: 1rem;
 `;
 
 export const Image = styled.img`
-    height: 1.5rem;
-    width: 3rem;
-    object-fit: contain;
+  height: 1.5rem;
+  width: 3rem;
+  object-fit: contain;
 `;
