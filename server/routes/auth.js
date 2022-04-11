fb08b8c6a8429fffeb8caf62f17927efebc59434
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-11 22:12:58 +0545

    refactor

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -23,7 +23,6 @@ router.post("/register", async (req, res) => {
 });
 
 // LOGIN
-
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
