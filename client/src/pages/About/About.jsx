7677c795f02a8cbc51c6eb025f1f3e132e485a4a
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-12 12:11:47 +0545

    feat(helmet integrated): title of the page added with react helmet

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import { Helmet } from "react-helmet";
 import { Announcements, AboutCompany } from "../../components/elements";
 import { Navbar, Newsletter, Footer } from "../../components/templates";
 
 const About = () => {
-    return (
-        <React.Fragment>
-            <Announcements />
-            <Navbar />
-            <AboutCompany />
-            <Newsletter />
-            <Footer />
-        </React.Fragment>
-    );
+  return (
+    <>
+      <Helmet>
+        <title>Alpha Male | About Us</title>
+      </Helmet>
+      <Announcements />
+      <Navbar />
+      <AboutCompany />
+      <Newsletter />
+      <Footer />
+    </>
+  );
 };
 
 export default About;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,27 +1,25 @@
-import React from "react";
 import { Announcements } from "../../components/elements";
-import {} from "../../components/layouts";
 import { Categories } from "../../components/modules";
 import {
-    Navbar,
-    Slider,
-    Newsletter,
-    NewArrivalCategory,
-    Footer,
+  Navbar,
+  Slider,
+  Newsletter,
+  NewArrivalCategory,
+  Footer,
 } from "../../components/templates";
 
 const Home = () => {
-    return (
-        <React.Fragment>
-            <Announcements />
-            <Navbar />
-            <Slider />
-            <Categories />
-            <NewArrivalCategory />
-            <Newsletter />
-            <Footer />
-        </React.Fragment>
-    );
+  return (
+    <>
+      <Announcements />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <NewArrivalCategory />
+      <Newsletter />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
