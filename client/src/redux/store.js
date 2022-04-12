f0e19e9702c8817567043f5608b9be36b15a07dd
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-12 16:08:31 +0545

    feat(redux-persist): persist created for userReducer and cartReducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
 
-export default configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: userReducer,
-  },
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
