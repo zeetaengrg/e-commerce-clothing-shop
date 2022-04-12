12f57382a18500f731af95bb3b13065ed3640fce
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-12 12:14:35 +0545

    refactor(export): exporting of unused components removed

--- client/src/components/modules/index.js
+++ client/src/components/modules/index.js
@@ -1,5 +1,2 @@
 export { default as Categories } from "./Categories/Categories";
-export { default as SignInModal } from "./Form/SignInModal/SignInModal";
-export { default as SignUpModal } from "./Form/SignUpModal/SignUpModal";
-export { default as SignInOutContainer } from "./Form/SignInOutContainer/SignInOutContainer";
 export { default as NewArrivals } from "./NewArrivals/NewArrivals";
