a222d92805cce7601dcaff8f4ab6c3a14e354715
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 00:34:24 +0545

    feat(markups): html tags markups added

--- admin/src/components/templates/Navbar/Navbar.jsx
+++ admin/src/components/templates/Navbar/Navbar.jsx
@@ -1,7 +1,34 @@
+import {
+  MdSearch,
+  MdLanguage,
+  MdNightlight,
+  MdLightMode,
+  MdNotifications,
+  MdMessage,
+  MdSettings,
+} from "react-icons/md";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <nav className="navbar">
+      <section className="section-one">
+        <input type="text" placeholder="Search" />
+        <MdSearch className="search__icon" />
+      </section>
+      <section className="section-two">
+        <div className="section-two__language">
+          <MdLanguage className="navbar__icon" />
+          English
+        </div>
+        <MdNightlight className="navbar__icon" />
+        <MdNotifications className="navbar__icon" />
+        <MdMessage className="navbar__icon" />
+        <img src="/images/profile-img.png" alt="Profile Pic" />
+        <MdSettings className="navbar__icon" />
+      </section>
+    </nav>
+  );
 };
 
 export default Navbar;
