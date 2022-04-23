df2e0022ef56a12217cb9ea5d0edb408fe4bc2ce
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 12:29:20 +0545

    feat(components): Widgets component imported and added to the markup

--- admin/src/pages/Home/Home.jsx
+++ admin/src/pages/Home/Home.jsx
@@ -1,13 +1,18 @@
-import { Navbar, Sidebar } from "components/templates";
+import { Navbar, Sidebar, Widgets } from "components/templates";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="home__content">
+      <div className="home-right">
         <Navbar />
-        Home Container
+        <div className="right-widgets">
+          <Widgets type="users" />
+          <Widgets type="orders" />
+          <Widgets type="earnings" />
+          <Widgets type="balance" />
+        </div>
       </div>
     </div>
   );
