6430c1bd2749eebc39cf468dd5c80f2fe1725e79
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 15:54:54 +0545

    feat(export): Table component exported

--- admin/src/components/templates/index.js
+++ admin/src/components/templates/index.js
@@ -3,3 +3,4 @@ export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Widgets } from "./Widgets/Widgets";
 export { default as RevenueChart } from "./RevenueChart/RevenueChart";
 export { default as GraphChart } from "./GraphChart/GraphChart";
+export { default as Table } from "./Table/Table";
