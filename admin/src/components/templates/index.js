0465addda15239943670a4e9ab960d62770532b5
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 19:18:29 +0545

    feat(export): component naming export added for ProductTable and UserData components

--- admin/src/components/templates/index.js
+++ admin/src/components/templates/index.js
@@ -3,4 +3,5 @@ export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Widgets } from "./Widgets/Widgets";
 export { default as RevenueChart } from "./RevenueChart/RevenueChart";
 export { default as GraphChart } from "./GraphChart/GraphChart";
-export { default as Table } from "./Table/Table";
+export { default as ProductTable } from "./ProductTable/ProductTable";
+export { default as UserData } from "./UserData/UserData";
