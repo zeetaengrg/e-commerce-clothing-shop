3eec6c8671d480c829c90bbe6beeb38fab7dff9d
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 15:52:12 +0545

    feat(markup): graph chart added

--- admin/src/components/templates/GraphChart/GraphChart.jsx
+++ admin/src/components/templates/GraphChart/GraphChart.jsx
@@ -10,70 +10,48 @@ import {
 import "./GraphChart.scss";
 
 const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  { name: "Janaury", Total: 1455.55 },
+  { name: "February", Total: 1150.95 },
+  { name: "March", Total: 1250.95 },
+  { name: "April", Total: 1350.95 },
+  { name: "May", Total: 850.95 },
+  { name: "June", Total: 1050.95 },
+  { name: "July", Total: 750.95 },
+  { name: "August", Total: 1250.95 },
+  { name: "September", Total: 1550.95 },
+  { name: "October", Total: 1950.95 },
+  { name: "November", Total: 2050.95 },
+  { name: "December", Total: 2150.95 },
 ];
 
 const GraphChart = () => {
   return (
     <div className="graph">
-      <ResponsiveContainer width="100%" height="100%">
+      <h1>Last Year (Revenue)</h1>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
         <AreaChart
-          width={500}
-          height={400}
+          width={730}
+          height={250}
           data={data}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" stroke="grey" />
+          {/* <YAxis /> */}
+          <CartesianGrid strokeDasharray="3 3" className="chart-grid" />
           <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#total)"
+          />
         </AreaChart>
       </ResponsiveContainer>
     </div>
