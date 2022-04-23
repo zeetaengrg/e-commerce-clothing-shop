6e0e8157e662aeae7a646d4843339f9d596ed795
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 14:38:51 +0545

    feat(export): RevenueChart and GraphChart added

--- admin/src/components/templates/index.js
+++ admin/src/components/templates/index.js
@@ -1,3 +1,5 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Widgets } from "./Widgets/Widgets";
+export { default as RevenueChart } from "./RevenueChart/RevenueChart";
+export { default as GraphChart } from "./GraphChart/GraphChart";
