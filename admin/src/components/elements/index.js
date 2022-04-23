d342b2ceadc893b6b37f75c18cce3d35ffed02cc
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 12:45:32 +0545

    feat(refactoring): unused exports removed

--- admin/src/components/elements/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Users } from "./Users/Users";
-export { default as Orders } from "./Orders/Orders";
-export { default as Earnings } from "./Earnings/Earnings";
-export { default as MyBalance } from "./MyBalance/MyBalance";
