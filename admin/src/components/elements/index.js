26ba88b3a18898942a8564f77bae39ab9ebf9117
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 12:31:39 +0545

    feat(directories): Widgets component created inside templates directory

---
+++ admin/src/components/elements/index.js
@@ -0,0 +1,4 @@
+export { default as Users } from "./Users/Users";
+export { default as Orders } from "./Orders/Orders";
+export { default as Earnings } from "./Earnings/Earnings";
+export { default as MyBalance } from "./MyBalance/MyBalance";
---
+++ admin/src/components/templates/Widgets/Widgets.jsx
@@ -0,0 +1,100 @@
+import {
+  MdArrowDropUp,
+  MdArrowDropDown,
+  MdAccountCircle,
+  MdShoppingCart,
+  MdAccountBalanceWallet,
+} from "react-icons/md";
+import { BiDollarCircle } from "react-icons/bi";
+import "./Widgets.scss";
+
+const Widgets = ({ type }) => {
+  let data;
+  let amount = 750;
+  let diff = 10;
+
+  switch (type) {
+    case "users":
+      data = {
+        title: "Users",
+        isMoney: false,
+        link: "See all Users",
+        icon: (
+          <MdAccountCircle
+            className="widget-icon"
+            style={{
+              color: "crimson",
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "orders":
+      data = {
+        title: "Orders",
+        isMoney: false,
+        link: "View all Orders",
+        icon: (
+          <MdShoppingCart
+            className="widget-icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218, 165, 32, 0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "earnings":
+      data = {
+        title: "Earnings",
+        isMoney: true,
+        link: "View Net Earnings",
+        icon: (
+          <BiDollarCircle
+            className="widget-icon"
+            style={{ color: "green", backgroundColor: "rgba(0, 255, 0, 0.2)" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "My Balance",
+        isMoney: true,
+        link: "View My Balance",
+        icon: (
+          <MdAccountBalanceWallet
+            className="widget-icon"
+            style={{ color: "purple", backgroundColor: "rgba(0,0,255, 0.2)" }}
+          />
+        ),
+      };
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div className="widgets">
+      <div className="widgets-top">
+        <h4>{data.title}</h4>
+        <div className="top-percentage increase">
+          <MdArrowDropUp />
+          <span>+{diff}%</span>
+        </div>
+      </div>
+      <div className="widgets-center">
+        {data.isMoney && "$ "}
+        {amount}
+      </div>
+      <div className="widgets-bottom">
+        <a href="/#">{data.link}</a>
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
+export default Widgets;
