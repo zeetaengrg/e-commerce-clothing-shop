8eefa9d7da919055d34ce76970e57d4eabf25389
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 19:20:44 +0545

    fix(naming): UsersList component name modified

--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as Home } from "./Home/Home";
-export { default as List } from "./List/List";
+export { default as UsersList } from "./UsersList/UsersList";
 export { default as Login } from "./Login/Login";
 export { default as New } from "./New/New";
 export { default as Single } from "./Single/Single";
