a94f9cd37dc58ec8ddb5021d203efc2c775df566
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-23 19:22:58 +0545

    feat(component & style): new component created and styled

---
+++ admin/src/pages/UsersList/UsersList.jsx
@@ -0,0 +1,16 @@
+import { Navbar, Sidebar, UserData } from "components/templates";
+import "./UsersList.scss";
+
+const UsersList = () => {
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="list-content">
+        <Navbar />
+        <UserData />
+      </div>
+    </div>
+  );
+};
+
+export default UsersList;
