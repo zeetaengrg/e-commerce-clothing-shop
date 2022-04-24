24705a525c1351fe234434037b8312e29afb1ded
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-24 22:08:42 +0545

    feat(markups): mapping of inputs from form data source added to display the form design

--- admin/src/pages/New/New.jsx
+++ admin/src/pages/New/New.jsx
@@ -38,60 +38,31 @@ const New = ({ inputs, title }) => {
                     style={{ cursor: "pointer", fontSize: "1.5rem" }}
                   />
                 </label>
-                <input type="file" id="file" style={{ display: "none" }} />
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
               </figcaption>
             </figure>
-            <form className="center">
-              <div className="form-control">
-                <label htmlFor="name">Full Name</label>
-                <input type="text" id="fullName" placeholder="Your Full Name" />
-              </div>
-              <div className="form-control">
-                <label htmlFor="phone">Phone</label>
-                <input type="tel" id="phone" placeholder="+01 234 567 890" />
-              </div>
-              <div className="form-control">
-                <label htmlFor="address">Address</label>
-                <div className="form-address">
-                  <input type="text" id="address" placeholder="Street No." />
-                  <input type="text" id="address" placeholder="City" />
-                  <input type="text" id="address" placeholder="Country" />
-                </div>
-              </div>
-            </form>
-            <form className="right">
-              <div className="form-control">
-                <label htmlFor="username">Username</label>
-                <input
-                  type="text"
-                  id="username"
-                  placeholder="Enter Your Username"
-                />
-              </div>
-              <div className="form-control">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" placeholder="Enter Your Email" />
-              </div>
-              <div className="form-control">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Enter Your Password"
-                />
-              </div>
-              <div className="form-control">
-                <label htmlFor="confirmPassword">Confirm Password</label>
-                <input
-                  type="password"
-                  id="confirmPassword"
-                  placeholder="Confirm Your Password"
-                />
-              </div>
-              <button className="btn" type="submit" onClick={handleClick}>
-                Submit
-              </button>
-            </form>
+            <div className="right">
+              <form>
+                {inputs.map((item) => (
+                  <div className="form-control" key={item.id}>
+                    <label htmlFor={item.ID}>{item.label}</label>
+                    <input
+                      type={item.type}
+                      id={item.ID}
+                      placeholder={item.placeholder}
+                    />
+                  </div>
+                ))}
+                <button className="btn" type="submit" onClick={handleClick}>
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         </section>
       </div>
