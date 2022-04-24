6f28dc616735e04821784a41641533837f0626dc
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-24 22:05:23 +0545

    feat(props): inputs and title props created to pass down the New component to render to users and products page respectively

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, UsersList, Login, New, Single } from "./pages";
+import { productInputs, userInputs } from "data/form";
 
 const App = () => {
   return (
@@ -12,12 +13,18 @@ const App = () => {
             <Route path="users">
               <Route index element={<UsersList />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
             </Route>
             <Route path="products">
               <Route index element={<UsersList />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
             </Route>
           </Route>
         </Routes>
