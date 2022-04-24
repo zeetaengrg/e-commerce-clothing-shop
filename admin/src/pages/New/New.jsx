158d6d71773d94024622aebab050ce5c361c1fd6
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-24 14:55:06 +0545

    style(margin): margin left set for right section

--- admin/src/pages/New/New.jsx
+++ admin/src/pages/New/New.jsx
@@ -14,7 +14,7 @@ const New = () => {
           </article>
         </section>
         <section className="bottom">
-          <article className="bottom-content">
+          <div className="bottom-content">
             <figure className="left">
               <img
                 src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
@@ -35,29 +35,73 @@ const New = () => {
               </div>
               <div className="form-control">
                 <label htmlFor="name">Full Name</label>
-                <input type="text" name="fullName" id="fullName" />
+                <input
+                  type="text"
+                  name="fullName"
+                  id="fullName"
+                  placeholder="Your Full Name"
+                />
               </div>
               <div className="form-control">
                 <label htmlFor="phone">Phone</label>
-                <input type="number" name="phone" id="phone" />
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  placeholder="+01 234 567 890"
+                />
               </div>
               <div className="form-control">
                 <label htmlFor="address">Address</label>
-                <input type="text" name="address" id="address" />
+                <div className="form-address">
+                  <input
+                    type="text"
+                    name="address"
+                    id="address"
+                    placeholder="Street No."
+                  />
+                  <input
+                    type="text"
+                    name="address"
+                    id="address"
+                    placeholder="City"
+                  />
+                  <input
+                    type="text"
+                    name="address"
+                    id="address"
+                    placeholder="Country"
+                  />
+                </div>
               </div>
             </form>
             <form className="right">
               <div className="form-control">
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username" />
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Enter Your Username"
+                />
               </div>
               <div className="form-control">
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter Your Email"
+                />
               </div>
               <div className="form-control">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Enter Your Password"
+                />
               </div>
               <div className="form-control">
                 <label htmlFor="confirmPassword">Confirm Password</label>
@@ -65,14 +109,14 @@ const New = () => {
                   type="password"
                   name="confirmPassword"
                   id="confirmPassword"
+                  placeholder="Confirm Your Password"
                 />
               </div>
-              <div className="form-control">
-                <label htmlFor="country">Country</label>
-                <input type="text" name="country" id="country" />
-              </div>
+              <button className="btn" type="submit">
+                Submit
+              </button>
             </form>
-          </article>
+          </div>
         </section>
       </div>
     </div>
