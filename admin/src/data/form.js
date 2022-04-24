a1134da211c181ba94e200195e23edcdad3ba9af
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-24 22:10:34 +0545

    feat(data): form data source created which contains userInputs and productInputs information

---
+++ admin/src/data/form.js
@@ -0,0 +1,78 @@
+export const userInputs = [
+  {
+    id: 1,
+    type: "text",
+    ID: "fullName",
+    label: "Full Name",
+    placeholder: "Enter Your Full Name",
+  },
+  {
+    id: 2,
+    type: "tel",
+    ID: "phone",
+    label: "Phone Number",
+    placeholder: "+01 234 567 890",
+  },
+  {
+    id: 3,
+    type: "text",
+    ID: "address",
+    label: "Address",
+    placeholder: ["Street", " City", " State", " Zip Code"],
+  },
+  {
+    id: 4,
+    type: "text",
+    ID: "username",
+    label: "Username",
+    placeholder: "Enter Your Username",
+  },
+  {
+    id: 5,
+    type: "email",
+    ID: "email",
+    label: "Email Address",
+    placeholder: "Enter Your Email Address",
+  },
+  {
+    id: 6,
+    type: "password",
+    ID: "password",
+    label: "Password",
+    placeholder: "Enter Your Password",
+  },
+  {
+    id: 7,
+    type: "password",
+    ID: "confirmPassword",
+    label: "Confirm Password",
+    placeholder: "Confirm Your Password",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Name of the Product",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Type of Product",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "Price of the Product",
+  },
+];
