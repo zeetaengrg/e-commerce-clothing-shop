08310f1ff174071e8c592792227252ceecdb2a09
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-26 19:32:26 +0545

    feat(markups added): necessary elements added to the markups

--- admin/src/pages/Login/Login.jsx
+++ admin/src/pages/Login/Login.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./Login.scss";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="login">
-      <div className="login-container">
+    <section className="login">
+      <form>
         <div className="form-control">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" placeholder="Enter Your Username" />
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter Your Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
@@ -14,10 +27,14 @@ const Login = () => {
             type="password"
             id="password"
             placeholder="Enter Your Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-      </div>
-    </div>
+        <button className="btn" type="submit" onClick={handleClick}>
+          Login
+        </button>
+      </form>
+    </section>
   );
 };
 
