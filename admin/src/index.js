14cdf8045531e8835669915091222f84fc81a821
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-26 19:29:53 +0545

    feat(import): packages and components imported and added
    - Provider imported from react-redux.
    - PersistGate imported from redux-persist.
    - store and persistor added from redux store.

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,14 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { DarkModeContextProvider } from "context/darkModeContext";
+import { store, persistor } from "redux/store";
+import App from "./App";
 import "./styles/global.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <DarkModeContextProvider>
-      <App />
-    </DarkModeContextProvider>
-  </React.StrictMode>
+  <DarkModeContextProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </DarkModeContextProvider>
 );
