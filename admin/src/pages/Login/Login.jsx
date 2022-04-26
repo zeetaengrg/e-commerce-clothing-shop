f5c5343bb627f954fc0c1a268bfa23970d6042ab
Jiten Gurung <zeetaen.gurung@gmail.com>
2022-04-26 19:06:31 +0545

    feat(login page): markups for login page added

--- admin/src/pages/Login/Login.jsx
+++ admin/src/pages/Login/Login.jsx
@@ -2,9 +2,22 @@ import "./Login.scss";
 
 const Login = () => {
   return (
-    <>
-      <div>Login</div>
-    </>
+    <div className="login">
+      <div className="login-container">
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" placeholder="Enter Your Username" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter Your Password"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
